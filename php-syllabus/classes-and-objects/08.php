3b85310710843ea299b44b4872018cc4a24d4d8b
Alexey Chikuns <alexey.chikuns@gmail.com>
2021-08-18 03:07:43 +0300

    fixed classes and objects 08

--- php-syllabus/classes-and-objects/08.php
+++ php-syllabus/classes-and-objects/08.php
@@ -22,10 +22,16 @@ class SavingAccount{
     }
 
     public function addAmountOfMonthInterest(): void{
-        $this->balance+= $this->annualInterestRate * $this->balance / 12;
+        $this->balance = $this->balance + ($this->balance * ($this->annualInterestRate / 12));
     }
 
 }
+//
+//function moneyFormat($money){
+//    $money = str_split((string) $money);
+//    array_unshift($money, '$');
+//    return join('',$money);
+//}
 
 $balance = (int) readline('How much money is in the account?: ');
 $annualInterestRate = (int) readline('Enter the annual interest rate: ');
@@ -33,29 +39,38 @@ $months = (int) readline('How long has the account been opened?: ');
 
 $savingsAccount = new SavingAccount($balance, $annualInterestRate);
 
-$deposits = 0;
-$withdraws = 0;
+$totalDeposits = 0;
+$totalWithdraws = 0;
 
 $interestEarned = 0;
 
+
+
 for($i = 1; $i <= $months; $i++){
 
-    $deposits += (int) readline("Enter amount deposited for month $i: ");
-    $withdraws += (int) readline("Enter amount withdrawn for month $i: ");
-    $balance =  $savingsAccount->balance;
+    $deposits = (int) readline("Enter amount deposited for month $i: ");
+    $withdraws = (int) readline("Enter amount withdrawn for month $i: ");
+
+    $totalDeposits+=$deposits;
+    $totalWithdraws+=$withdraws;
+
+    $savingsAccount->subtractAmountOfWithdrawl($withdraws);
+    $savingsAccount->addAmountOfDeposit($deposits);
+
+    $oldBalance =  $savingsAccount->balance;
     $savingsAccount->addAmountOfMonthInterest();
-    $interestEarned+=$savingsAccount->balance - $balance;
+    $interestEarned+=($savingsAccount->balance - $oldBalance);
+
 
 }
-$savingsAccount->subtractAmountOfWithdrawl($withdraws);
-$savingsAccount->addAmountOfDeposit($deposits);
+
 
 
 echo "Total deposited: $deposits" . PHP_EOL;
 echo "Total withdrawn: $withdraws" . PHP_EOL;
 
-echo "Interest earned: " . $interestEarned . PHP_EOL;
-echo "Ending balance: $savingsAccount->balance" . PHP_EOL;
+echo "Interest earned: " . round($interestEarned, 2) . PHP_EOL;
+echo "Ending balance: " . round($savingsAccount->balance,2 ) . PHP_EOL;
 
 
 
